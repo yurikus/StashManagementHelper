ebe0fa6f729a6d72fa8e61f6ea1be1d078297a6e
Markosz <mrksz22@gmail.com>
2025-04-07 23:26:58 +0200

    ItemTypes

--- SortingStrategy/ItemTypes.cs
+++ SortingStrategy/ItemTypes.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using EFT.InventoryLogic;
 
 namespace StashManagementHelper;
@@ -34,48 +33,34 @@ public static class ItemTypes
         Backpacks,
         Info,
         HeadgearArmor,
-        Unknown
     }
 
-    // Optimized type checking with refined order based on likely inheritance
-    public static ItemType GetItemTypeEnum(Item item) => item switch
+    public static readonly Dictionary<ItemType, Func<Item, bool>> ItemTypeMap = new()
     {
-        // Most specific subtypes first
-        ArmoredEquipmentItemClass => ItemType.HeadgearArmor,
-        BackpackItemClass => ItemType.Backpacks,
-        VestItemClass => ItemType.Rigs,
-        SpecialScopeItemClass => ItemType.SpecialEquipment,
-        SpecialWeaponItemClass => ItemType.SpecialEquipment,
-        VisorsItemClass => ItemType.Eyewear,
-        HeadwearItemClass => ItemType.Headgear,
-        FaceCoverItemClass => ItemType.Facecovers,
-        HeadphonesItemClass => ItemType.Headsets,
-        MagazineItemClass => ItemType.Magazines,
-        AmmoBox => ItemType.Ammo,
-        MedsItemClass => ItemType.Meds,
-        FoodItemClass => ItemType.Food,
-        DrinkItemClass => ItemType.Drink,
-        KnifeItemClass => ItemType.Melee,
-        ThrowWeapItemClass => ItemType.Grenades,
-        KeyItemClass => ItemType.Keys,
-        RepairKitsItemClass => ItemType.RepairKits,
-        ArmorPlateItemClass => ItemType.BallisticPlates,
-        MoneyItemClass => ItemType.Money,
-        InfoItemClass => ItemType.Info,
-        BarterItemItemClass => ItemType.Barter,
-
-        // More general types later
-        Weapon => ItemType.Weapons,
-        ArmorItemClass => ItemType.Armor,
-        Mod => ItemType.Mods,
-        AmmoItemClass => ItemType.Ammo,
-        SimpleContainerItemClass => ItemType.Containers,
-
-        _ => ItemType.Unknown
+        {ItemType.Weapons, item => item is Weapon},
+        {ItemType.Armor, item => item is ArmorItemClass},
+        {ItemType.Magazines, item => item is MagazineItemClass},
+        {ItemType.Ammo, item => item is AmmoItemClass or AmmoBox},
+        {ItemType.Meds, item => item is MedsItemClass},
+        {ItemType.Food, item => item is FoodItemClass},
+        {ItemType.Drink, item => item is DrinkItemClass},
+        {ItemType.Melee, item => item is KnifeItemClass},
+        {ItemType.Mods, item => item is Mod},
+        {ItemType.Grenades, item => item is ThrowWeapItemClass},
+        {ItemType.Barter, item => item is BarterItemItemClass},
+        {ItemType.Rigs, item => item is VestItemClass},
+        {ItemType.Eyewear, item => item is VisorsItemClass},
+        {ItemType.Headgear, item => item is HeadwearItemClass},
+        {ItemType.Facecovers, item => item is FaceCoverItemClass},
+        {ItemType.Headsets, item => item is HeadphonesItemClass},
+        {ItemType.Keys, item => item is KeyItemClass},
+        {ItemType.Containers, item => item is SimpleContainerItemClass },
+        {ItemType.Backpacks, item => item is BackpackItemClass },
+        {ItemType.RepairKits, item => item is RepairKitsItemClass},
+        {ItemType.SpecialEquipment, item => item is SpecialScopeItemClass or SpecialWeaponItemClass},
+        {ItemType.BallisticPlates, item => item is ArmorPlateItemClass},
+        {ItemType.Money, item => item is MoneyItemClass},
+        {ItemType.Info, item => item is InfoItemClass},
+        {ItemType.HeadgearArmor, item => item is ArmoredEquipmentItemClass},
     };
-
-    public static readonly List<ItemType> AllItemTypes = Enum.GetValues(typeof(ItemType))
-                                                            .Cast<ItemType>()
-                                                            .Where(it => it != ItemType.Unknown)
-                                                            .ToList();
 }
